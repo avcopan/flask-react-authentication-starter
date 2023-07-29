5131faf1be3d60e48dbeeb80fd015128e08305f9
Andreas Copan <avcopan@gmail.com>
2023-07-28 21:18:44 -0400

    Sets up styling with TailwindCSS/DaisyUI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,9 @@ import fireIcon from "/fire-icon.svg";
 
 export default function Header() {
   return (
-    <div>
-      <div>
-        <img src={fireIcon} className="logo" alt="Vite logo" />
-      </div>
-      <h1>Flask-React Auth Starter</h1>
+    <div className="flex flex-row gap-6 items-center">
+      <img className="h-32" src={fireIcon} alt="Fire icon" />
+      <h1 className="text-4xl">Flask-React Auth Starter</h1>
     </div>
-  )
+  );
 }
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,7 +2,7 @@ import Header from "./Header";
 
 export default function Layout({ children }) {
   return (
-    <div>
+    <div className="p-8 min-h[100vh] bg-base-100">
       <header>
         <Header />
       </header>
